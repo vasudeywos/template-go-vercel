eb5caef4f2659c0f62a83356c0304880da02ce35
aditya <adidoodle25@gmail.com>
2023-09-21 11:30:50 +0530

    bj

--- api/json.go
+++ api/json.go
@@ -4,20 +4,54 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"github.com/xuri/excelize/v2"
 )
-
+type Mess struct {
+	Date      string   `json:"date"`
+	Breakfast []string `json:"breakfast"`
+	Lunch     []string `json:"lunch"`
+	Dinner    []string `json:"dinner"`
+}
 func Json(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusCreated)
-	w.Header().Set("Content-Type", "application/json")
-	resp := make(map[string]string)
-	resp["message"] = "Hello World from Go! 👋"
-	resp["language"] = "go"
-	resp["cloud"] = "Hosted on Vercel! ▲"
-	resp["github"] = "https://github.com/riccardogiorato/template-go-vercel/blob/main/api/json.go"
-	jsonResp, err := json.Marshal(resp)
+	f, _ := excelize.OpenFile("ssms_test.xlsx")
+
+	rows, _ := f.GetCols("Sheet1")
+
+	var messArr []Mess
+
+	for i, row := range rows {
+		if i == 0 {
+			continue
+		}
+		var b []string
+		var l []string
+		var d []string
+		for j := 3; j < 12; j++ {
+			b = append(b, row[j])
+		}
+		for n := 14; n <= 20; n++ {
+			l = append(l, row[n])
+		}
+		for k := 24; k <= 30; k++ {
+			d = append(d, row[k])
+		}
+
+		mess := Mess{
+			Date:      row[1],
+			Breakfast: b,
+			Lunch:     l,
+			Dinner:    d,
+		}
+		messArr = append(messArr, mess)
+	}
+
+	jsonResp, err := json.Marshal(messArr)
 	if err != nil {
 		fmt.Printf("Error happened in JSON marshal. Err: %s", err)
-	} else {
-		w.Write(jsonResp)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
 	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jsonResp)
 }
